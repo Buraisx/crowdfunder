ae19d99cdc425cfaad50dfbc7a4bfb52782eb331
Situ William <situ_william@yahoo.com>
2017-12-13 11:55:16 -0500

    added 404 pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   private
   def not_authenticated
     redirect_to login_path, alert: "Please login first"
   end
 
-   def current_user
-      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def not_found
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 
   helper_method :current_user
